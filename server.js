8ab85ef9e9310af113acfb78945f008b527855d7
Eric Blackham <erict.blackham@gmail.com>
2018-12-05 14:51:53 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
 const cron = require('node-cron');
 const users = require('./routes/api/users');
 const birthday = require('./routes/api/birthday');
@@ -45,6 +47,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/birthday', birthday);
 // app.use('/api/posts', posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // ********* SEND AUTOMATIC REMINDERS
 
