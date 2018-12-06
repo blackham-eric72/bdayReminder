f213acfc3dd55b6714e749f406908371e8751427
Eric Blackham <erict.blackham@gmail.com>
2018-12-06 16:49:31 -0700

    added composeWithDevTools to see if that fixes the crashing...

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers =
-  // (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  //   window.__REDUX_DEVTOOLS_EXTENSION__()) ||
-  compose;
+// const composeEnhancers =
+//   // (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+//   //   window.__REDUX_DEVTOOLS_EXTENSION__()) ||
+//   compose;
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 // const store = createStore(
